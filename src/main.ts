8964d89afa78060283e34b4d3027b1d9924dada6
Jhon Moofarrif <jhon.moofarrifgmail.com>
2024-06-17 00:21:14 -0500

    add log port

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,8 @@ async function bootstrap() {
     }),
   );
 
+  console.log("App is running on port: ", process.env.PORT ?? 3000)
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

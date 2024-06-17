1647e54bd39dc3fb886a45b026185ac23490b5b9
Jhon Moofarrif <jhon.moofarry00@gmail.com>
2024-06-17 00:08:05 -0500

    auth app

---
+++ README.md
@@ -0,0 +1,9 @@
+# NestJS backend
+
+```sh
+docker compose up -d
+```
+
+
+copiar le ```.env.template``` y renombrarlo a ```.env```
+.
---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      dbName: process.env.MONGO_DB_NAME,
+    }),
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
